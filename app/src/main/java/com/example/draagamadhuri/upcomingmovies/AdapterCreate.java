e88986697d3d6f3133c151766cf8ccb783547c5d
JackAlexandr <ee16b007@smail.iitm.ac.in>
2017-06-29 23:27:27 +0530

    modified

--- app/src/main/java/com/example/draagamadhuri/upcomingmovies/AdapterCreate.java
+++ app/src/main/java/com/example/draagamadhuri/upcomingmovies/AdapterCreate.java
@@ -1,8 +1,68 @@
 package com.example.draagamadhuri.upcomingmovies;
 
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by D.RAAGA MADHURI on 28-Jun-17.
  */
 
-public class AdapterCreate {
+public class AdapterCreate extends RecyclerView.Adapter<AdapterCreate.ViewHolder> {
+
+    public ArrayList<String> names_list;
+    public ArrayList<Drawable> image_list;
+    public ArrayList<String> dates_list;
+
+    public AdapterCreate(ArrayList<String> names, ArrayList<Drawable> images, ArrayList<String> dates){
+        names_list = names;
+        image_list = images;
+        dates_list = dates;
 }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_type, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.item_name.setText(names_list.get(position));
+        holder.item_date.setText(dates_list.get(position));
+        holder.item_background.setImageDrawable(image_list.get(position));
+
+        holder.item_background.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return names_list.size();
+    }
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    ImageView item_background;
+    TextView item_name;
+    TextView item_date;
+    public ViewHolder(View itemView) {
+        super(itemView);
+
+        item_name = (TextView) itemView.findViewById(R.id.textView);
+        item_background = (ImageView) itemView.findViewById(R.id.image);
+        item_date = (TextView) itemView.findViewById(R.id.textView3);
+    }
+}
+}
+
--- app/src/main/java/com/example/draagamadhuri/upcomingmovies/MainActivity.java
+++ app/src/main/java/com/example/draagamadhuri/upcomingmovies/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.draagamadhuri.upcomingmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView list = (RecyclerView)findViewById(R.id.list);
+        list.setItemAnimator(new DefaultItemAnimator());
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Spiderman Homecoming");
+        names.add("Dunkirk");
+        names.add("The Emoji Movie");
+        names.add("Justice League");
+        names.add("Thor Ragnarok");
+        names.add("War For The Planet Of The Apes");
+
+        ArrayList<Drawable> images = new ArrayList<>();
+        images.add(getResources().getDrawable(R.drawable.homecoming));
+        images.add(getResources().getDrawable(R.drawable.dunkirk));
+        images.add(getResources().getDrawable(R.drawable.emoji));
+        images.add(getResources().getDrawable(R.drawable.justice));
+        images.add(getResources().getDrawable(R.drawable.thor));
+        images.add(getResources().getDrawable(R.drawable.planet));
+
+        ArrayList<String> dates = new ArrayList<>();
+        dates.add("07/07/2017");
+        dates.add("21/07/2017");
+        dates.add("28/07/2017");
+        dates.add("16/11/2017");
+        dates.add("25/10/2017");
+        dates.add("14/07/2017");
+
+        AdapterCreate myAdapter = new AdapterCreate(names, images, dates);
+
+        list.setAdapter(myAdapter);
+
     }
 }
